e111006b0489b9f024ac7674e13b190631422df9
Leyman Max <jiumoh2011@yandex.ru>
2023-06-12 23:36:44 +0300

    refactoring

--- img_utils.py
+++ img_utils.py
@@ -1,7 +1,7 @@
 from PIL.Image import Image
 
 
-def center_crop(img: Image, new_width=None, new_height=None) -> Image:
+def center_crop(img: Image, new_width: int = None, new_height: int = None) -> Image:
     width, height = img.size
 
     if new_width is None:
@@ -10,9 +10,9 @@ def center_crop(img: Image, new_width=None, new_height=None) -> Image:
     if new_height is None:
         new_height = min(width, height)
 
-    left = (width - new_width) / 2
-    top = (height - new_height) / 2
-    right = (width + new_width) / 2
-    bottom = (height + new_height) / 2
+    left = int((width - new_width) / 2)
+    top = int((height - new_height) / 2)
+    right = int((width + new_width) / 2)
+    bottom = int((height + new_height) / 2)
 
     return img.crop((left, top, right, bottom))
